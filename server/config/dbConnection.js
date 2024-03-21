2e4ee17413a42c8f2557d1712fb9d98071fcf2ff
Haseebsheikh786 <125917762+Haseebsheikh786@users.noreply.github.com>
2024-03-21 19:03:12 +0500

    Add mongodb CONNNECTION_STRING

--- server/config/dbConnection.js
+++ server/config/dbConnection.js
@@ -2,11 +2,14 @@ const mongoose = require("mongoose");
 
 const connectDb = async () => {
   try {
-    const connect = await mongoose.connect(process.env.CONNNECTION_STRING);
+    const connect = await mongoose.connect(
+      process.env.CONNNECTION_STRING ||
+        "mongodb+srv://haseeb99sh:saadjaan@cluster0.gx1b9rv.mongodb.net/auth?retryWrites=true&w=majority"
+    );
     console.log("Database connected");
   } catch (err) {
     console.log(err);
     process.exit(1);
   }
 };
-module.exports = connectDb;
+module.exports = connectDb; 
